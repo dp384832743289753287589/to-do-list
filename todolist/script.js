081e8b02a344b9cb242c3f593aa7a4d32e54b9fb
unknown <darsheagles@gmail.com>
2025-11-14 19:07:05 -0500

    sorry

---
+++ todolist/script.js
@@ -0,0 +1,45 @@
+//it was easier this way instead of redeclaring variables
+function ManageTasks(e){
+    e.preventDefault();
+    let date = new Date();
+    let div=document.getElementById('container');
+    let input=document.getElementById('taskinput');
+    let task = document.createElement('div');
+    let checkbox= document.createElement('input');
+    let removeBtn = document.createElement('button');
+    let taskdiv=document.createElement('div');
+    let completiondate=document.createElement('input')
+
+    taskdiv.setAttribute('class', 'task-div');
+    removeBtn.setAttribute('class', 'remove-btn');
+    removeBtn.innerText = 'X';
+    checkbox.setAttribute('class', 'checkbox');
+    checkbox.setAttribute('type', 'checkbox');
+    task.setAttribute('class', 'task');
+    task.innerText = input.value;
+    completiondate.setAttribute('type', 'date');
+    completiondate.setAttribute('class', 'completion-date');
+    let taskamount=document.createElement('p');
+    let numberoftasks=document.getElementsByClassName('task').length;
+    taskamount.setAttribute('class', 'task-amount');
+    taskamount.innerText = "Task "+(numberoftasks+1)+" "+"Created: "+date.toLocaleString();
+    if (input.value === ''){
+        alert("Task cannot be empty");
+        return;
+    }else{
+        div.appendChild(taskdiv);
+        taskdiv.appendChild(task);
+        taskdiv.appendChild(removeBtn);
+        taskdiv.appendChild(checkbox);
+        taskdiv.appendChild(completiondate);
+        taskdiv.appendChild(taskamount);
+
+    }
+
+
+    removeBtn.addEventListener('click', function(){
+        div.removeChild(taskdiv);
+    });
+}
+document.getElementById('addtask').addEventListener('click', ManageTasks);
+
